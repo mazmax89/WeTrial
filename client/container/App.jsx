9254e64a83b20d824aea37b12e9e81037ae34fbe
Mazmax <mazmax89@gmail.com>
2017-11-03 23:11:03 +0100

    router-animation

--- client/container/App.jsx
+++ client/container/App.jsx
@@ -1,16 +1,26 @@
+import './AppStyle.scss';
 import React, {Component} from 'react';
-import Home from './Home';
-import Counter from '../container/CounterApp';
 import Route from 'react-router-dom/es/Route';
 import Header from '../components/Header';
+import Home from './Home';
+import CounterApp from './CounterApp';
+import {AnimatedSwitch} from 'react-router-transition';
+
 
 export default class App extends Component {
     render() {
         return (
             <div>
                 <Header/>
-                <Route path='/' exact component={Home}/>
-                <Route path='/Counter' component={Counter}/>
+                <AnimatedSwitch
+                    atEnter={{marginLeft: -500}}
+                    atLeave={{marginLeft: -500}}
+                    atActive={{marginLeft: 0}}
+                    className='switch-wrapper'
+                >
+                    <Route path='/' exact component={Home}/>
+                    <Route path='/Counter' component={CounterApp}/>
+                </AnimatedSwitch>
             </div>
         );
     }
---
+++ client/container/Home.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className='row justify-content-center'>
+                <div className='col-xs-12 col-md-4'>
+                    <h1>Home</h1>
+                    <p>Welcome to the world of trials!</p>
+                </div>
+            </div>
+        );
+    }
+}
